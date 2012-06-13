d8407057099ae20752054dda79cad52c232f68f8
Fredrik Sundstrom <fredrik.sundstrom@gmail.com>
2012-06-13 13:50:17 +0200

    Added render

--- controllers/base_controller.php
+++ controllers/base_controller.php
@@ -4,6 +4,17 @@ namespace WpMvc
 {
   class BaseController
   {
-    
+    public static function render( $controller, $action )
+    {
+      $controller_class = get_class( $controller );
+
+      $controller_name = \WpMvc\Application::rename_controller_class_to_file( $controller_class );
+      $controller_dir = preg_replace( "/_controller/", "", $controller_name );
+      $action_file = $action . ".php";
+
+      $full_path = \WpMvc\Config::$home_path . "/views/" . $controller_dir . "/" . $action_file;
+
+      include( $full_path );
+    }
   }
 }
