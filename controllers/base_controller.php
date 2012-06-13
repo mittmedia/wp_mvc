4f4a829431aa9778a050cc6c523d06a979036d65
Fredrik Sundstrom <fredrik.sundstrom@gmail.com>
2012-06-13 16:51:05 +0200

    Updated base controller

--- controllers/base_controller.php
+++ controllers/base_controller.php
@@ -9,10 +9,10 @@ namespace WpMvc
       $controller_class = get_class( $controller );
 
       $controller_name = \WpMvc\Application::rename_controller_class_to_file( $controller_class );
-      $controller_dir = preg_replace( "/_controller/", "", $controller_name );
-      $action_file = $action . ".php";
+      $controller_dir = preg_replace( '/_controller/', '', $controller_name );
+      $action_file = $action . '.php';
 
-      $full_path = \WpMvc\Config::$home_path . "/views/" . $controller_dir . "/" . $action_file;
+      $full_path = \WpMvc\Config::$home_path . '/views/' . $controller_dir . '/' . $action_file;
 
       include( $full_path );
     }
