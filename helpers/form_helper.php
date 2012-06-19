8afdc641d3b4a73e36f022c125ccd86349e73fe4
Fredrik Sundstrom <fredrik.sundstrom@gmail.com>
2012-06-19 16:34:39 +0200

    Updated FormHelper to deal with sub classes

--- helpers/form_helper.php
+++ helpers/form_helper.php
@@ -4,39 +4,42 @@ namespace WpMvc
 {
   class FormHelper
   {
-    protected $object;
-    protected $class_name;
-
-    public static function render_form( $object, $content, $action = null )
+    public static function render_form( $object, $content, $action = null, $verb = 'create' )
     {
       if ( $action == null )
         $action = $_SERVER['REQUEST_URI'];
 
-      $this->object = $object;
-
       $class_name = get_class( $object );
-      $this->class_name = strtolower( $class_name );
+      $class_name_lowered = strtolower( $class_name );
 
       $html = "<form action='$action' method='post'>";
 
-      foreach ( $content as $name => $type ) {
-        $html .= $this->form_element( $name, $type );
+      foreach ( $content as $name => $type_or_value ) {
+        if ( $name == 'object' ) {
+          $sub_object = $type_or_value['object'];
+          $sub_class_name = get_class( $sub_object );
+          $sub_class_name_lowered = strtolower( $sub_class_name );
+
+          $html .= static::form_element( $type_or_value['name'], $type_or_value['type'], array( $class_name_lowered, $sub_class_name_lowered ), array( $object, $sub_object ), $type_or_value['default_value'], $type_or_value['key'] );
+        } else {
+          $html .= static::form_element( $name, $type_or_value, $class_name_lowered, $object );
+        }
       }
 
-      $html .= $this->default_actions();
+      $html .= static::default_actions( $verb );
 
       $html .= "</form>";
 
       echo $html;
     }
 
-    public static function form_element( $name, $type )
+    public static function form_element( $name, $type, $class_name, $object, $default_value = null, $key = null )
     {
       $html = "";
 
       switch ( $type ) {
         case 'text':
-          $html = $this->input_text( $name );
+          $html = static::input_text( $name, $class_name, $object, $default_value, $key );
           break;
         case 'textarea':
           $html = $this->input_textarea( $name );
@@ -46,12 +49,12 @@ namespace WpMvc
       return $html;
     }
 
-    public static function input_text( $name )
+    public static function input_text( $name, $class_name, $object, $default_value, $key )
     {
-      $class_name = $this->class_name;
-      $object_value = $this->object->{$name};
-
-      return "<input type='text' name='{$class_name}[{$name}]' id='{$class_name}_{$name}' class='standard-text' value='$object_value' />";
+      if ( is_array( $class_name ) || is_array( $object )  )
+        return "<input type='text' name='{$class_name[0]}[{$class_name[1]}][{$name}]" . ( $key ? "[{$key}]" : '' ) . "' id='{$class_name[0]}_{$class_name[1]}_{$name}' class='standard-text' value='$default_value' />";
+      else
+        return "<input type='text' name='{$class_name}[{$name}]' id='{$class_name}_{$name}' class='standard-text' value='" . ( $default_value ? $default_value : $object->{$name} ) . "' />";
     }
 
     public static function input_textarea( $name )
@@ -62,9 +65,16 @@ namespace WpMvc
       return "<textarea name='{$class_name}[{$name}]' id='{$class_name}_{$name}' class='standard-text'>$object_value</textarea>";
     }
 
-    public static function default_actions()
+    public static function default_actions( $verb )
     {
-      return "<p class='submit'><input type='submit' name='submit' id='submit' class='button-primary' value='Update'></p>";
+      switch ( $verb ) {
+        case 'create':
+          return "<p class='submit'><input type='submit' name='submit' id='submit' class='button-primary' value='" . __( 'Create' ) . "'></p>";
+          break;
+        case 'update':
+          return "<p class='submit'><input type='submit' name='submit' id='submit' class='button-primary' value='" . __( 'Update' ) . "'></p>";
+          break;
+      }
     }
   }
 }
