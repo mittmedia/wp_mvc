54f60686e44364837008a890cc24acfc0810bdf3
Fredrik Sundstrom <fredrik.sundstrom@gmail.com>
2012-06-19 13:47:01 +0200

    refactored relation

--- models/blog.php
+++ models/blog.php
@@ -6,11 +6,9 @@ class Blog extends \WpMvc\BaseModel
   public static $class_name = 'Blog';
   public static $id_column = 'blog_id';
   public $options;
-  public $option;
 
   public function init()
   {
-    $this->options = \Option::find_by_blog_id( $this->{static::$id_column} );
-    self::populate_sub_class( &$this->options, &$this->option );
+    self::has_many( $this->options, \Option::find_by_blog_id( $this->{static::$id_column} ) );
   }
 }
