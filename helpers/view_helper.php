7126f1d3e03f7948cb843615afae268e4a7dd6c6
Fredrik Sundstrom <fredrik.sundstrom@gmail.com>
2012-06-21 11:27:29 +0200

    Updated home path

--- helpers/view_helper.php
+++ helpers/view_helper.php
@@ -8,7 +8,7 @@ namespace WpMvc
     {
       $partial_path_splitted = explode( '/', $path );
       $partial_name = '_' . array_pop( $partial_path_splitted ) . '.php';
-      $partial_path = \WpMvc\Config::$home_path;
+      $partial_path = \WpMvc\Config::$application_path;
       $partial_path .= '/views/';
       $partial_path .= implode( '/', $partial_path_splitted );
       $partial_path .= '/';
@@ -21,7 +21,7 @@ namespace WpMvc
     {
       $template_path_splitted = explode( '/', $path );
       $template_name = array_pop( $template_path_splitted ) . '.php';
-      $template_path = \WpMvc\Config::$home_path;
+      $template_path = \WpMvc\Config::$application_path;
       $template_path .= '/views/';
       $template_path .= implode( '/', $template_path_splitted );
       $template_path .= '/';
