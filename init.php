59b51fe4f2e7518ae2269b52157d8f2eeb00f66c
Fredrik Sundstrom <fredrik.sundstrom@gmail.com>
2012-06-27 09:38:06 +0200

    Fixes bug

--- init.php
+++ init.php
@@ -1,18 +1,18 @@
 <?php
 
-require_once( 'wp_mvc-1.0.0/controllers/base_controller.php' );
+require_once( 'wp_mvc/controllers/base_controller.php' );
 
-require_once( 'wp_mvc-1.0.0/helpers/application_helper.php' );
-require_once( 'wp_mvc-1.0.0/helpers/dev_helper.php' );
-require_once( 'wp_mvc-1.0.0/helpers/form_helper.php' );
-require_once( 'wp_mvc-1.0.0/helpers/view_helper.php' );
+require_once( 'wp_mvc/helpers/application_helper.php' );
+require_once( 'wp_mvc/helpers/dev_helper.php' );
+require_once( 'wp_mvc/helpers/form_helper.php' );
+require_once( 'wp_mvc/helpers/view_helper.php' );
 
-require_once( 'wp_mvc-1.0.0/models/base_model.php' );
-require_once( 'wp_mvc-1.0.0/models/blog.php' );
-require_once( 'wp_mvc-1.0.0/models/option.php' );
-require_once( 'wp_mvc-1.0.0/models/site.php' );
-require_once( 'wp_mvc-1.0.0/models/site_meta.php' );
-require_once( 'wp_mvc-1.0.0/models/user.php' );
-require_once( 'wp_mvc-1.0.0/models/user_meta.php' );
+require_once( 'wp_mvc/models/base_model.php' );
+require_once( 'wp_mvc/models/blog.php' );
+require_once( 'wp_mvc/models/option.php' );
+require_once( 'wp_mvc/models/site.php' );
+require_once( 'wp_mvc/models/site_meta.php' );
+require_once( 'wp_mvc/models/user.php' );
+require_once( 'wp_mvc/models/user_meta.php' );
 
-require_once( 'wp_mvc-1.0.0/application.class.php' );
+require_once( 'wp_mvc/application.class.php' );
