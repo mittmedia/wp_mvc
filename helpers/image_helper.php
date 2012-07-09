5b9e9c949f9fc69207c83aa3c0ef13a933e5f877
Fredrik Sundstrom <fredrik.sundstrom@gmail.com>
2012-07-09 17:17:47 +0200

    Code structure

--- helpers/image_helper.php
+++ helpers/image_helper.php
@@ -13,22 +13,22 @@ namespace WpMvc
 
       $image = $image_path . $file;
 
-      list($imagewidth, $imageheight, $imagetype) = getimagesize($image);
-      $imagetype = image_type_to_mime_type($imagetype);
+      list( $imagewidth, $imageheight, $imagetype ) = getimagesize( $image );
+      $imagetype = image_type_to_mime_type( $imagetype );
 
-      if(($imageheight > $fixed_height) && ($imagetype == "image/x-png" || $imagetype == "image/png")) {
+      if ( ( $imageheight > $fixed_height ) && ( $imagetype == "image/x-png" || $imagetype == "image/png" ) ) {
         $scale =  $fixed_height / $imageheight;
-        $new_image = imagecreatetruecolor(floor($imagewidth * $scale), $fixed_height);
-        $source = imagecreatefrompng($image);
-        imagecopyresampled($new_image, $source, 0, 0, 0, 0, $imagewidth * $scale, $fixed_height, $imagewidth, $imageheight);
+        $new_image = imagecreatetruecolor( floor( $imagewidth * $scale ), $fixed_height);
+        $source = imagecreatefrompng( $image );
+        imagecopyresampled( $new_image, $source, 0, 0, 0, 0, $imagewidth * $scale, $fixed_height, $imagewidth, $imageheight );
 
-        if ( $options["save_image"] == true) {
-          imagepng($new_image, $image_path . $new_filename);
-          chmod($image_path . $new_filename, 0777);
+        if ( $options["save_image"] == true ) {
+          imagepng( $new_image, $image_path . $new_filename );
         } else {
           return $new_image;
         }
       }
+
       return true;
     }
   }
